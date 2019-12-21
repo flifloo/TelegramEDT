c20f2509bbe1bcf5a7271eee1fcbeb29a97a665a
flifloo <flifloo@gmail.com>
2019-12-21 11:49:06 +0100

    Update requirements and add update script

---
+++ update.py
@@ -0,0 +1,9 @@
+import shelve
+from EDTuser import User
+
+with shelve.open("edt", writeback=True) as db:
+    for u in db:
+        nu = User(0, None)
+        for v in db[u].__dict__:
+            setattr(nu, v, db[u].__dict__[v])
+        db[u] = nu
